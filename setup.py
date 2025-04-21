0e33de610758eb26b6c0f5429cfa0e4be2a2a19c
me <yonosoyosorio@gmail.com>
2025-04-21 17:33:29 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ThirthRepoRealeases",                     # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Daniel Osorio",                         # Tu nombre
+    author_email="yonosoyosorio@gmail.com",         # Tu correo electrónico
+    url="https://github.com/2rns4s/thirthRepoReleases",     # URL del proyecto
+)
